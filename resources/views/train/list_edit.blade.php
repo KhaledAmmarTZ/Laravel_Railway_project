1701b5566f4e9f789c5c6113e0b1aeffdd48ee1b
Khaled Ammar <khaledammartz@gmail.com>
2025-02-09 20:07:37 +0600

    updated 9/2/2025 8:07

--- resources/views/train/list_edit.blade.php
+++ resources/views/train/list_edit.blade.php
@@ -3,18 +3,18 @@
 @section('title', 'Select Train to Edit')
 
 @section('content')  
-    @foreach ($trains as $train)
     <div class="card text-center" style="width: 100%; background-color: #f8f9fa; border: 1px solid #ccc;">
         <div class="card-header  text-white" style="background-color: #005F56">
                 <h2>Select a Train to Edit</h2>
         </div>
         <div class="card-body">
             <div class="mb-3 d-flex align-items-center">
+            @foreach ($trains as $train)
                 <li>
                     <a href="{{ route('train.edit', $train->trainid) }}">{{ $train->trainname }}</a>
                 </li>
+             @endforeach    
             </div>
         </div>
     </div>
-    @endforeach
 @endsection
