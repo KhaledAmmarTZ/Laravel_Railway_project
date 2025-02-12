b3a51c25bffb943e8e009cf5e3e751e6161f53dc
Khaled Ammar <khaledammartz@gmail.com>
2025-02-12 21:18:08 +0600

    updated 12/2/2025 9:18

--- resources/views/admin/admin_profile.blade.php
+++ resources/views/admin/admin_profile.blade.php
@@ -1,11 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extend(layout.admin)
+@section('title')
+    Admin Profile
+@endsection
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Admin Profile') }}</div>
+
+                    <div class="card-body"> </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
