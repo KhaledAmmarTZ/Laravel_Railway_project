55c2c6e85397f25690f800ff56a67f85f2a82e2a
Khaled Ammar <khaledammartz@gmail.com>
2025-04-09 17:56:37 +0600

    change to laravel 12

--- routes/web.php
+++ routes/web.php
@@ -1,50 +1,22 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\TrainController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\RouteController;
-use App\Http\Controllers\PdfController;
-use App\Http\Controllers\StationController;
 use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+    Volt::route('settings/password', 'settings.password')->name('settings.password');
+    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 });
-
-Route::resource('routes', RouteController::class);
-Route::get('/routes/{route}/edit', [RouteController::class, 'edit'])->name('routes.edit');
-Route::put('/routes/{route}', [RouteController::class, 'update'])->name('routes.update');
-
-Route::get('/stations', [TrainController::class, 'getStations']);
-Route::get('/train_route',function (){
-    return view('train.train_route');
-});
-Route::get('/trains', function () {
-    $trains = \App\Models\Train::all(['trainname']);  // Fetch only the train name
-    return response()->json($trains);
-});
-
-Route::get('/user', [UserController::class, 'showProfile']);
-
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-// Route accessible by everyone (train.index)
-Route::get('/train', [TrainController::class, 'index'])->name('train.index');
-
-
-
 
 require __DIR__.'/auth.php';
-
-require __DIR__.'/admin-auth.php';
